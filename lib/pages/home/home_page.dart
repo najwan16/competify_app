fed2b9511f285716c089b27bb0da78a01186103e
najwan16 <naje1612@gmail.com>
2025-03-14 15:39:41 +0700

    navbar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:competify_app/pages/home/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_fonts/google_fonts.dart';
